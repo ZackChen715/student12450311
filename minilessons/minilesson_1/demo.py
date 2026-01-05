d2c6738791f0b499e636bfc5f03a0ae45b9eb0fa
alexlan <lanxi222cn@outlook.com>
2026-01-05 01:02:59 -0600

    minilesson 1

---
+++ minilessons/minilesson_1/demo.py
@@ -0,0 +1,6 @@
+# Demo Python script for Mini-Lesson 1
+# Run this with: python3 demo.py
+# Print a message
+print("This is a simple Python script!")
+print("You can run it from the command line using: python3 demo.py")
+
